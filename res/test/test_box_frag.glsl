f1e54a8d901e9f790a5c529642f5a9e859bb4cff
Sebastian Hegardt <se.hegardt@gmail.com>
2020-04-03 09:23:19 +0900

    New tests shaders and addded hsv shader

--- res/test/test_box_frag.glsl
+++ res/test/test_box_frag.glsl
@@ -1,5 +1,14 @@
 #version 120
 
-void main() {
+uniform vec2 size;
 
+in vec3 vert_pos;
+out vec4 frag_color;
+
+#import "pattern.glsl"
+
+void main() {
+    vec3 bg_color = vec3(1.0,1.0,1.0);
+    vec3 box_color = vec3(0.,0.,0.);
+    frag_color = vec4(mix(bg_color, box_color, box(vert_pos.xy, size, 0.0)), 1.0);
 }
--- res/test/test_smoothstep_frag.glsl
+++ res/test/test_smoothstep_frag.glsl
@@ -1,11 +1,12 @@
 #version 430
 
+uniform vec2 edge0;
+uniform vec2 edge1;
+
 in vec3 vert_pos;
 out vec4 frag_color;
 
 void main() {
-    float edge0 = 0.5;
-    float edge1 = 1.0;
-    float red = smoothstep(edge0, edge1, vert_pos.x); // interpolate red for 0.5<x<1.0
-    frag_color = vec4(red, 0.5,0.,1.0);
+    vec2 red = smoothstep(edge0, edge1, vert_pos.xy); // interpolate red for 0.5<x<1.0
+    frag_color = vec4(red, 0.,1.0);
 }
