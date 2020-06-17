954441d5c5ca891954c097ca16d4da689c37c108
Sebastian Hegardt <se.hegardt@gmail.com>
2020-06-17 17:20:11 +0200

    Removed unused functions and marked deprecated functions

--- src/misc/render_funcs.py
+++ src/misc/render_funcs.py
@@ -1,6 +1,7 @@
+import logging
 import typing
+import warnings
 
-import autograd.numpy as anp
 import numpy as np
 import torch
 
@@ -8,21 +9,21 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
 CHANNELS = 3
 
+logger_ = logging.getLogger(__name__)
 
-# def input(width: int, height: int, f: typing.Callable, *call_dict):
-#     img = np.zeros((width, height, 4))
-#
-#     x_res = 1.0 / width
-#     y_res = 1.0 / height
-#     x_pos = np.linspace(0, 1.0, width, endpoint=False) + (x_res / 2.0)
-#     y_pos = np.linspace(0, 1.0, height, endpoint=False) + (y_res / 2.0)
-#
-#     for input in range(width):
-#         for y in range(height):
-#             frag_pos = anp.array((x_pos[input], y_pos[y], 0.))
-#             img[y, input, :] = f(frag_pos, *call_dict)
-#
-#     return img
+
+def render_iter(width: int, height: int, f: typing.Callable, *args):
+    warnings.warn("Iterative rendering method is deprecated!", DeprecationWarning, stacklevel=2)
+    img = np.zeros((height, width, CHANNELS))
+    x_pos, y_pos = get_coordinates(width, height)
+
+    for row in range(height):
+        for col in range(width):
+            frag_pos = torch.tensor((x_pos[col], y_pos[row], 0.), dtype=torch.float32)
+            val = f(frag_pos, *args)
+            img[height - 1 - row, col, :] = val
+
+    return img
 
 
 def get_coordinates(width: int, height: int) -> typing.Tuple[torch.Tensor, torch.Tensor]:
@@ -33,52 +34,7 @@ def get_coordinates(width: int, height: int) -> typing.Tuple[torch.Tensor, torch
     return x_pos, y_pos
 
 
-def render_torch(width: int, height: int, f: typing.Callable, *args):
-    img = np.zeros((height, width, CHANNELS))
-    x_pos, y_pos = get_coordinates(width, height)
-
-    for row in range(height):
-        for col in range(width):
-            frag_pos = torch.tensor((x_pos[col], y_pos[row], 0.), dtype=torch.float32)
-            val = f(frag_pos, *args)
-            img[height - 1 - row, col, :] = val
-
-    return img
-
-
-def render_torch_loop(width: int, height: int, f: typing.Callable, *args):
-    img = torch.zeros((width, height, CHANNELS), device=device)
-    x_pos, y_pos = get_coordinates(width, height)
-
-    for row in range(height):
-        for col in range(width):
-            frag_pos = torch.tensor((x_pos[col], y_pos[row], 0.), dtype=torch.float32)
-            val = f(frag_pos, *args)
-            img[col, row,:] = val
-
-    return img
-
-
 def generate_frag_pos(width: int, height: int) -> torch.Tensor:
-    x_pos, y_pos = torch.meshgrid((get_coordinates(width, height)))
+    x_pos, y_pos = torch.meshgrid(*get_coordinates(width, height))
     frag_pos = torch.stack([x_pos, y_pos, torch.zeros_like(x_pos)], dim=2)
-    return frag_pos
-
-def render_torch_matrix(width: int, height: int, f, *args):
-    x_pos, y_pos = torch.meshgrid((get_coordinates(width, height)))
-    pix_pos = torch.stack([x_pos, y_pos, torch.zeros_like(x_pos)], dim=2)
-    return f(pix_pos, *args)
-
-
-def render_torch_with_callback(width: int, height: int, row_callback: typing.Callable, f: typing.Callable, *args):
-    img = np.zeros((height, width, CHANNELS))
-    x_pos, y_pos = get_coordinates(width, height)
-
-    for row in range(height):
-        row_callback(row)
-        for col in range(width):
-            frag_pos = torch.tensor((x_pos[col], y_pos[row], 0.), dtype=torch.float32)
-            val = f(frag_pos, *args)
-            img[height - 1 - row, col, :] = val
-
-    return img
+    return frag_pos.float()
