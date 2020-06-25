d45c0a1e59ae314ab9280e4265f41e56951f6c06
Sebastian Hegardt <se.hegardt@gmail.com>
2020-06-25 21:46:16 +0200

    Added helpter function

--- src/node_graph/data_type.py
+++ src/node_graph/data_type.py
@@ -2,9 +2,18 @@ import enum
 
 
 # ----- Internal Types --------
+
+
 class DataType(enum.Enum):
     Float = "type_float"
     Int = "type_int"
     Vec3_Float = "type_vec3_float"
     Vec3_RGB = "type_vec3_rgb"
     Shader = "type_shader"
+
+
+def dtype_size(dtype: DataType):
+    if "vec3" in dtype:
+        return 3
+    else:
+        return 1
